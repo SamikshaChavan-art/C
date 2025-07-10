d6b39a146e7a2c843196b2bc585d9041330352e2
Samiksha <samikshachavan@gmail.com>
2025-07-10 15:22:35 +0530

    Union, Structure and File Handling Basics

--- structure/fileunion.c
+++ structure/fileunion.c
@@ -5,21 +5,30 @@ struct Student
     char name[10];
     float perc;
 };
-
 union Employee
 {
     int empid;
     char empname[10];
-    int sal;
+    float sal;
 };
 
 int main()
 {
     struct Student stud;
     union Employee emp;
+    FILE *fp;
+
+    fp = fopen("E:\\output.txt", "w");
+    if (fp == NULL)
+    {
+        printf("Error Opening file");
+        return 1;
+    }
 
     printf("Sizeof Student %d", sizeof(stud));
     printf("\nSizeof Employee %d", sizeof(emp));
+    fprintf(fp, "\nSize of Student is %d", sizeof(stud));
+    fprintf(fp, "\nSize of employee is %d", sizeof(emp));
 
     printf("\nEnter Student Roll No:");
     scanf("%d", &stud.rollno);
@@ -30,19 +39,19 @@ int main()
     printf("\nEnter Student Percentage:");
     scanf("%f", &stud.perc);
 
-    printf("\nStudent Roll No is %d \n", stud.rollno);
-    printf("Student Name is %s \n", stud.name);
-    printf("Student Percentage is %.2f \n", stud.perc);
+    fprintf(fp,"\nStudent Roll No is %d \n", stud.rollno);
+    fprintf(fp,"\nStudent Name is %s \n", stud.name);
+    fprintf(fp,"\nStudent Percentage is %.2f \n", stud.perc);
 
     printf("\nEnter Employee ID:");
     scanf("%d", &emp.empid);
-    printf("\nEmployee ID is %d \n", emp.empid);
+    fprintf(fp,"\nEmployee ID is %d \n", emp.empid);
 
     printf("\nEnter Employee Name:");
     scanf("%s", &emp.empname);
-    printf("Employee Name is %s \n", emp.empname);
+    fprintf(fp,"\nEmployee Name is %s \n", emp.empname);
 
     printf("\nEnter Salary:");
     scanf("%d", &emp.sal);
-    printf("Employee Salary is %d \n", emp.sal);
+    fprintf(fp,"\nEmployee Salary is %d \n", emp.sal);
 }
